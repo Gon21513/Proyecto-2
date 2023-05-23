fc37d1cd4d36d81f56e70183abf23854a0e64c76
Gon2153 <125921214+Gon21513@users.noreply.github.com>
2023-05-22 22:10:30 -0600

    Servos 4
    Se agragaron algunas funciones para elegi el modo y de lectura y escritura en la eeprom

--- Ptoyectofinal.X/main.c
+++ Ptoyectofinal.X/main.c
@@ -39,7 +39,7 @@
 #define tmr0_val 249
 unsigned int CCPRA = 0; //variable para el ccpr1
 unsigned int CCPRB = 0; //variable para el ccpr2
-
+uint8_t bandera; //bandera para elegir los modos
 
 //-----------------prototipos-----------------
 void setup(void); //prototipo de setuo
@@ -90,10 +90,37 @@ void __interrupt() isr(void){
         }
         PIR1bits.ADIF = 0; //limpia la bandera del adc
     }
+    
+    //interrupcion del tmr0
     if (INTCONbits.T0IF == 1){
         manualPWM_ISR();
     }
+    
+    //interrupcion del portb
+    if (INTCONbits.RBIF){//revisar las interrupciones del portb
+        if (PORTBbits.RB0 == 0){//enceder
+            PORTDbits.RD2 = 1; //limpia el bits que inica el sleeps
+            PORTDbits.RD3 = 1; //limpia el bits que inica el sleeps
+
+        }
+        
+        else if (PORTBbits.RB1 == 0){
+            PORTDbits.RD2 = 0 ; //encender un led para indicarme que sleep esta encdedio
+            PORTDbits.RD3 = 1; //limpia el bits que inica el sleeps
+
+        }
+        
+        else if (PORTBbits.RB2 == 0){
+            PORTDbits.RD2 = 1; //apagar led que indica si esta dormido 
+            PORTDbits.RD3 = 0; //limpia el bits que inica el sleeps
+
+        }
+        INTCONbits.RBIF = 0; 
+
     }
+    return;
+
+}
 
 
 //////////main
@@ -149,15 +176,31 @@ void setup(void){
     TRISAbits.TRISA2 = 1; //puerto A1 como entrada (era este)
     TRISAbits.TRISA3 = 1; //puerto A1 como entrada (era este)
     TRISAbits.TRISA4 = 1; //puerto A1 como entrada (era este)
-
+    
+    //botones
+    TRISBbits.TRISB0 = 1; //rb0 como entrada
+    TRISBbits.TRISB1 = 1; //rb1 como entrada 
+    TRISBbits.TRISB2 = 1; //rb1 como entrada 
     
     TRISCbits.TRISC3 = 0; //puerto C3 como salida (era este)
-    TRISCbits.TRISC4 = 0; //puerto C3 como salida (era este)
+    TRISCbits.TRISC4 = 0; //puerto C4 como salida (era este)
+    TRISE = 0x00; 
+    TRISD = 0x00; 
 
     
     // --------------- limpiar puertos --------------- 
     PORTA = 0;
+    PORTB = 0;
     PORTC = 0;
+    PORTE = 0;
+    PORTD = 0;
+    
+    
+    //----------pullups------------------
+    OPTION_REGbits.nRBPU = 0; //habilitarr pullups
+    WPUBbits.WPUB0 = 1;
+    WPUBbits.WPUB1 = 1; 
+    WPUBbits.WPUB2 = 1; 
  
     
     // --------------- Oscilador --------------- 
@@ -170,6 +213,14 @@ void setup(void){
 
     INTCONbits.GIE = 1; // habilitar interrupciones globales
     INTCONbits.PEIE = 1; // habilitar interrupciones perifericas
+    INTCONbits.RBIE = 1; //habilitar interrupciones en portb
+    
+    IOCBbits.IOCB0 = 1; //habilitar interrupciones en rb0
+    IOCBbits.IOCB1 = 1; // habilitar interrupciones en rb1
+    IOCBbits.IOCB2 = 1; // habilitar interrupcion en el rb2
+
+    
+    INTCONbits.RBIF = 0; //limpirar bander de interrupcion de portb
     PIE1bits.ADIE = 1; // habilitar interrupciones de ADC
     PIR1bits.ADIF = 0; // limpiar la bandera de interrupcion del ADC
 }
@@ -262,6 +313,7 @@ unsigned short cambiopwm(uint8_t valor, uint8_t POTMIN, uint8_t POTMAX,
 }
 
 
+//multiplexado para controlar los dos servos adicionales 
 void manualPWM_ISR(void) {
         PORTCbits.RC3 = 1; //encender led
         delay(pot3); // delay (tiempo en alto del pulso)
@@ -280,5 +332,45 @@ void delay(unsigned int micro){
     while (micro > 0){
         __delay_us(250); //delay de 0.25ms
         micro--; //decrementar variable
+    }
+}
+
+//----------funciones para eeprom-----------------------
+
+void EEPROMWRITE(uint8_t address, uint8_t data){
+    EEADR = address;//asignar direccin de datos 
+    EEDAT = data;//datos 
+    
+    EECON1bits.EEPGD = 0; //escribe en la memoria de datos
+    EECON1bits.WREN = 1; // habilita escritura en eeprom 
+    
+    INTCONbits.GIE = 0; //deshabilita las interrupciones 
+    
+
+    //obligatorio
+    EECON2 = 0x55;
+    EECON2 = 0xAA;
+    EECON1bits.WR = 1; //habilitar escritua
+    
+    EECON1bits.WREN = 0; //apagamos la escritura
+    
+    INTCONbits.RBIF = 0; // limpiamos bandera en el puerto b
+    INTCONbits.GIE = 1; //habilita interrupciones globales 
+            
+}
+
+uint8_t EEPROMREAD(uint8_t address){
+    EEADR = address ;//asgina la direccin 
+    EECON1bits.EEPGD = 0;//selecciona la menoria eeprom
+    EECON1bits.RD = 1;//habilita lectura de eeprom
+    return EEDAT;//retorna el valor de la direccion leida
+}
+
+//Funcion para mostrar texto
+void cadena(char *cursor){
+    while (*cursor != '\0'){//mientras el cursor sea diferente a nulo
+        while (PIR1bits.TXIF == 0); //mientras que se este enviando no hacer nada
+            TXREG = *cursor; //asignar el valor del cursor para enviar
+            *cursor++;//aumentar posicion del cursor
     }
 }
